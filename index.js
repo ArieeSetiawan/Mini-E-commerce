4f2154f255cc9da9072a109903f462272a01353b
arieesetiawan <arieesetiawann@gmail.com>
2022-08-23 13:23:51 +0700

    adding socket chat

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const logger = require ('morgan');
 const swaggerUI = require('swagger-ui-express');
 const server = require('http').Server(app);
 const cors = require ('cors');
-const io = require ('socket.io')(server,{cors:{origin:"*"}})
+const io = require ('socket.io')(server,{
+  cors:{origin:"*"},
+})
 const errHandler = require ('./middlewares/errHandler')
 
 require('dotenv').config();
@@ -31,9 +33,31 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc, {
     }
   }));
 
+//////////
+
+const rooms = { }
 app.get('/chat',(req,res)=>{
-  res.render('chat')
+  res.render('chat', {rooms:rooms})
 })
+
+
+app.post('/room', (req, res) => {
+  if (rooms[req.body.room] != null) {
+    return res.redirect('/chat')
+  }
+  rooms[req.body.room] = { users: {} }
+  res.redirect(req.body.room)
+  io.emit('room-created', req.body.room)
+})
+
+app.get('/:room', (req, res) => {
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/chat')
+  }
+  res.render('room', { roomName: req.params.room })
+})
+
+///////////
 
 app.use('/users', userRoutes);
 app.use('/sellers', sellerRoutes);
@@ -47,20 +71,29 @@ server.listen(process.env.PORT, ()=>{
     console.log("Server is running on port", process.env.PORT);
 })
 
-const users = {}
-io.on('connection', (socket) => {
-  socket.on('new-user', name => {
-    users[socket.id] = name
-    socket.broadcast.emit('user-connected', name)
+io.on('connection', socket => {
+  socket.on('new-user', (room, name) => {
+    socket.join(room)
+    rooms[room].users[socket.id] = name
+    socket.to(room).emit('user-connected', name)
   })
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  socket.on('send-chat-message', (room, message) => {
+    socket.to(room).emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
   })
   socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id])
-    delete users[socket.id]
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id]
+    })
   })
 })
+
+function getUserRooms(socket) {
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
+}
 
 module.exports = app;
 module.exports = server;
