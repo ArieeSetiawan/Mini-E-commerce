f2da2191d50acccfef6dc4a7b9de63fb395dbe16
arieesetiawan <arieesetiawann@gmail.com>
2022-09-01 14:43:20 +0700

    fixing seller test

--- test/seller.test.js
+++ test/seller.test.js
@@ -8,74 +8,17 @@ const server = require('../index')
 const testUser = {
     firstName: 'tester123',
     lastName: 'tester123',
-    email: 'tester123@mail.com',
+    email: 'test-arie@yopmail.com',
     password: 'Tester123',
     address: "Jakarta",
 }
 
-const testUser1 = {
-    firstName: 'tester1234',
-    lastName: 'tester1234',
-    email: 'tester1234@mail.com',
-    password: 'Tester1234',
-    address: "Jakarta",
-}
 
 let validToken = '';
-let invalidToken = 'Invalid-token';
-let tokenforDelete = '';
 
 afterAll(() => {
   server.close()
 });
-
-describe('Seller Delete Endpoints',()=>{
-  it('POST /sellers/register with valid values, response should be 201', async () => {
-    const res = await request(app)
-      .post('/sellers/register')
-      .send(testUser1)
-      .set('Accept', 'application/json');
-
-    expect(res.status).toBe(201);
-    // expect(typeof res.body.message).toMatch('string');
-  })
-
-  it('POST /sellers/login with valid email and pass, response should be 200', async () => {
-    const res = await request(app)
-      .post('/sellers/login')
-      .set('Accept', 'application/json')
-      .send({
-        email: testUser1.email,
-        password: testUser1.password
-      });
-    expect(res.status).toBe(200);
-    expect(res.body).toHaveProperty('token');
-    expect(typeof res.body.token).toMatch('string');
-    tokenforDelete = res.body.token;
-  })
-
-  it('Delete /sellers/id with valid token, response should be 200.', async () => {
-    const user = jwt.verify(tokenforDelete,process.env.SECRET_KEY);
-    const parameter = user.id
-    const response = await request(app)
-      .delete('/sellers/'+ parameter)
-      .set('Accept', 'application/json')
-      .set('authorization', tokenforDelete)
-
-    expect(response.status).toEqual(200);
-  })
-
-  it('Delete /sellers/id with invalid token, response should be 200.', async () => {
-    const user = jwt.verify(tokenforDelete,process.env.SECRET_KEY);
-    const parameter = user.id
-    const response = await request(app)
-      .delete('/sellers/'+ parameter)
-      .set('Accept', 'application/json')
-      .set('authorization', "invalidtoken")
-
-    expect(response.status).toEqual(401);
-  })
-})
 
 afterAll(() => {
   seller.destroy({
@@ -217,6 +160,28 @@ describe('Seller Endpoints', () => {
       })
 
     expect(response.status).toEqual(500);
+  })
+
+  it('Delete /sellers/id with valid token, response should be 200.', async () => {
+    const user = jwt.verify(validToken,process.env.SECRET_KEY);
+    const parameter = user.id
+    const response = await request(app)
+      .delete('/sellers/'+ parameter)
+      .set('Accept', 'application/json')
+      .set('authorization', validToken)
+
+    expect(response.status).toEqual(200);
+  })
+
+  it('Delete /sellers/id with invalid token, response should be 500.', async () => {
+    const user = jwt.verify(validToken,process.env.SECRET_KEY);
+    const parameter = user.id
+    const response = await request(app)
+      .delete('/sellers/'+ parameter)
+      .set('Accept', 'application/json')
+      .set('authorization', "invalidtoken")
+
+    expect(response.status).toEqual(401);
   })
   
 })
